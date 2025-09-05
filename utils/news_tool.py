a0d21a73ecba809e4e82244f9eb49d6e90a18c20
Kuan <rueikuanwei@gmail.com>
2025-09-05 00:01:22 -0700

    finished news tool

--- utils/news_tool.py
+++ utils/news_tool.py
@@ -1,101 +1,142 @@
-from llama_index.core import SimpleDirectoryReader 
-from news_extractors import NewsExtractor
 from datetime import datetime
-from news_puller import pull_general_news, pull_company_news
+import json
+# import news_puller
+from . import news_puller
+from .news_extractors import NewsExtractor
+from llama_index.core import SimpleDirectoryReader, VectorStoreIndex, Response, StorageContext
+# from news_extractors import NewsExtractor
 from llama_index.core.tools import QueryEngineTool
 from llama_index.core.query_engine import RouterQueryEngine
 from llama_index.core.selectors import LLMSingleSelector
-from llama_index.core.response.pprint_utils import pprint_response
-import json
+from llama_index.vector_stores.chroma import ChromaVectorStore
 import chromadb
+from llama_index.core.response.pprint_utils import pprint_response
 from llama_index.llms.ollama import Ollama
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 
-chroma_client = chromadb.PersistentClient('./chromadb')
+client = chromadb.PersistentClient('./chromadb_storage')
 
-def has_loaded(sym=None):
-    current_time = datetime.now().strftime("%Y-%m-%d")
-    if sym:
+def pull_updated_news(symbol):
+    general_persist=False
+    company_persist=False
+    with open('./utils/data/general_news.json', 'r') as file:
+        data = json.load(file)
+        timestamp = data['timestamp']
+        
+        curr_time = datetime.now().strftime('%Y-%m-%d')
+        general_persist=curr_time != timestamp
+        if general_persist:
+            news_puller.pull_general_news()
+
+    if symbol:
         with open('./utils/data/company_news.json', 'r') as file:
             data = json.load(file)
-            timestamp = data.get('timestamp')
-            symbol = data.get('symbol')
-        return current_time == timestamp and sym == symbol
-    
-    with open('./utils/data/general_news.json', 'r') as file:
-        timestamp = json.load(file).get('timestamp')
-        return current_time == timestamp
-
-def add_document_to_chroma(collection, document, embed_model):
-    for doc in document:
-        headline = doc.extra_info.get('headline', '')
-        summary = doc.extra_info.get('summary', '')
-        related = doc.extra_info.get('related', '')
-        doc_id = doc.extra_info.get('id', '')
-
-    combined_text = f"{headline}\n\n{summary}\n\n{related}"
-
-    collection.add(
-        document=[combined_text],
-        metadata=[doc.extra_info],
-        ids=[str(doc_id)],
-        embeddings=embed_model.encode([combined_text])
-    )
-
-def ingest_news(embed_model, symbol=None):
-    print('Checking for new news...')
-    if not has_loaded(sym=symbol):
-        print('New news not loaded, pulling data...')
-        if symbol:
-            pull_company_news(symbol=symbol)
-        pull_general_news()
+            timestamp = data['timestamp']
+            sym = data['symbol']
         
-        print("Reading new documents...")
-        general_doc = SimpleDirectoryReader(input_files=["./utils/data/general_news.json"],
-                                            file_extractor={".json", NewsExtractor()})
-        company_doc = SimpleDirectoryReader(input_files=["./utils/data/company_news.json"],
-                                            file_extractor={".json", NewsExtractor()})
+        curr_time = datetime.now().strftime('%Y-%m-%d')
+        company_persist=curr_time != timestamp or sym != symbol
+        if company_persist:
+            news_puller.pull_company_news(symbol=symbol)
 
-        print("Adding documents to ChromaDB collections...")
-        general_collection = chroma_client.get_or_create_collection('general_collection')
-        company_collection = chroma_client.get_or_create_collection('company_collection')
+    return general_persist, company_persist
 
-        add_document_to_chroma(general_collection, general_doc, embed_model=embed_model)
-        add_document_to_chroma(company_collection, company_doc, embed_model=embed_model)
-    else:
-        print("Collections are up-to-date. Skipping ingestion.")
+def load_docs_and_index(embed_model, general_persist, company_persist):
+        indices = []
+        
+        general_collection = client.get_or_create_collection(name='general')
+        general_docs = SimpleDirectoryReader(
+            input_files=["./utils/data/general_news.json"],
+            file_extractor={".json": NewsExtractor()}
+        ).load_data()
+        vector_store = ChromaVectorStore(chroma_collection=general_collection)
+        storage_context = StorageContext.from_defaults(vector_store=vector_store)
+        
+        if general_persist:
+            index = VectorStoreIndex.from_documents(
+                general_docs,
+                storage_context=storage_context,
+                embed_model=embed_model
+            )
+        else:
+            index = VectorStoreIndex.from_vector_store(
+                vector_store=vector_store, 
+                storage_context=storage_context,
+                embed_model=embed_model
+            )
+
+        indices.append(index)
+
+        company_collection = client.get_or_create_collection(name='company')
+        company_docs = SimpleDirectoryReader(
+            input_files=["./utils/data/company_news.json"],
+            file_extractor={".json": NewsExtractor()}
+        ).load_data()
+        vector_store = ChromaVectorStore(chroma_collection=company_collection)
+        storage_context = StorageContext.from_defaults(vector_store=vector_store)
+        
+        if company_persist:
+            index = VectorStoreIndex.from_documents(
+                company_docs,
+                storage_context=storage_context,
+                embed_model=embed_model
+            )
+        else:
+            index = VectorStoreIndex.from_vector_store(
+                vector_store=vector_store,
+                storage_context=storage_context,
+                embed_model=embed_model
+            )
+        indices.append(index)
+
+        return indices
 
 def get_news_info(query: str, llm, embed_model, symbol=None) -> str:
-
-    general_collection = chroma_client.get_or_create_collection('general_collection')
-    company_collection = chroma_client.get_or_create_collection('company_collection')
-
+    if not llm or not embed_model:
+        raise Exception('No llm or embed model')
+    
+    general_persist, company_persist = pull_updated_news(symbol=symbol)
+    indices = load_docs_and_index(embed_model=embed_model, 
+                                  general_persist=general_persist,
+                                  company_persist=company_persist)
+    
+    if len(indices) != 2:
+        raise Exception('No indexes loaded')
+    
+    general_query_engine = indices[0].as_query_engine(llm=llm)
     general_tool = QueryEngineTool.from_defaults(
-        query_engine=general_collection,
-        description='General Market News'
+        query_engine=general_query_engine,
+        description='Using general market news to provide response'
     )
-    company_tool = QueryEngineTool.from_defaults(
-        query_engine=company_collection,
-        description='Company Specific News'
-    )
+    query_engine_tools = [general_tool]
 
+    if symbol:
+        company_query_engine = indices[1].as_query_engine(llm=llm)
+        company_tool = QueryEngineTool.from_defaults(
+            query_engine=company_query_engine,
+            description='Targeting a specific company news to provide response'
+        )
+        query_engine_tools.append(company_tool)
+    
     query_engine = RouterQueryEngine(
         selector=LLMSingleSelector.from_defaults(llm=llm),
-        query_engine_tools=[general_tool, company_tool],
+        query_engine_tools=query_engine_tools,
         llm=llm
     )
 
-    res = query_engine.query(query)
+    res: Response = query_engine.query(query)
+    pprint_response(response=res, show_source=True)
+
     return res.response
-              
-
-
-def main():
-    llm = Ollama(model='qwen3:8b', request_timeout=600)
-    embed_model = HuggingFaceEmbedding('BAAI/bge-base-en-v1.5')
-    ingest_news(embed_model=embed_model, symbol='AAPL')
-    print(get_news_info('What is the best stock to by?', llm=llm))
     
+def main():
+    pull_updated_news('TSLA')
+    llm = Ollama(model='llama3.2:1b', request_timeout=600)
 
-if __name__ == "__main__":
+    embed_model = HuggingFaceEmbedding('BAAI/bge-base-en-v1.5')
+    res = get_news_info('What companies is the main headline today?', llm=llm, embed_model=embed_model, symbol=None)
+
+    print(f"Final result: {res}")
+
+if __name__ == '__main__':
     main()
